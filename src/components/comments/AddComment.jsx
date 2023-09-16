6b68d76ee18381c4f1059a0041b4b361e514cf4a
Motaz Ramadan <120125123+motazramadan8@users.noreply.github.com>
2023-09-16 21:02:56 +0300

    Update AddComment.jsx

--- src/components/comments/AddComment.jsx
+++ src/components/comments/AddComment.jsx
@@ -16,6 +16,8 @@ const AddComment = ({ postId }) => {
       return toast.warn("Comment Is Required", toastOptions);
 
     dispatch(createComment({ text, postId}))
+    window.location.reload()
+    window.location.reload()
     setText("")
   };
   return (
