506cecc8f670b31f8b74690ddc0921f05930dd62
motazramadan8 <motazramadan198@gmail.com>
2023-09-22 22:38:17 +0300

    Repair PostItem

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "https://devister.onrender.com",
+  // baseURL: "https://devister.onrender.com",
+  baseURL: "http://localhost:8000",
+
 });
 
 export default request;
